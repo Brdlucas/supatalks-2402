805c9dfd48d37da7f8c3f2fe1fcdc5fbe3055ed5
LucasPort <lb.lucasbourdon@gmail.com>
2025-01-23 11:01:36 +0100

    Appelle du repository Post pour pouvoir récupérer les éléments dans la table avec un findAll. Affichage des posts dans le rendu avec une limitation de 5 posts.

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/posts', name: 'app_post')]
-    public function index(): Response
+    #[Route('/posts', name: 'app_post', methods: ['GET'])] // Method get pour seulement récupérer les informations
+    public function posts(PostRepository $pr): Response
     {
+
+        $posts = $pr->findAll();
         return $this->render('page/posts.html.twig', [
-            'controller_name' => 'PageController',
+            'posts' => $posts,
+            5
         ]);
     }
 }
