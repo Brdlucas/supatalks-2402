d7acc8e814b21ea6e66ba4532d110c022f896884
LucasPort <lb.lucasbourdon@gmail.com>
2025-01-23 10:35:16 +0100

    Ajout de la création de 200 utilisateurs et 200 posts dans les fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Post;
 use Faker\Factory;
 use App\Entity\Event;
 use App\Entity\Speaker;
@@ -20,8 +21,21 @@ class AppFixtures extends Fixture
         $user->setEmail('admin@admin.fr')
             ->setPassword('$2y$13$NJpGg/WaTYG0ONkZkf6tvuPVmkuexwRQqozQKsp5b8yc9z9B3ziMG') // admin
             ->setRoles(['ROLE_ADMIN'])
-            ;
+        ;
         $manager->persist($user);
+
+
+        for ($i = 0; $i < 200; $i++) {
+            $user = new User();
+            $user->setEmail($faker->email());
+            $user->setPassword('$2y$13$NJpGg/WaTYG0ONkZkf6tvuPVmkuexwRQqozQKsp5b8yc9z9B3ziMG');
+            $user->setRoles(['ROLE_USER']);
+
+
+            // Permet de créer une référence pour pouvoir l'utiliser dans la loop posts
+            $this->addReference('user_' . $i, $user);
+            $manager->persist($user);
+        }
 
         // Création de 40 speakers
         $speakerImages = [
@@ -30,8 +44,9 @@ class AppFixtures extends Fixture
             'user3.jpg',
             'user4.jpg',
         ];
+
         $speakerArray = [];
-        for ($i=1; $i < 41; $i++) { 
+        for ($i = 1; $i < 41; $i++) {
             $speaker = new Speaker();
             $speaker->setFirstname($faker->firstName)
                 ->setLastname($faker->lastName)
@@ -39,9 +54,9 @@ class AppFixtures extends Fixture
                 ->setCompany($faker->company)
                 ->setExperience($faker->numberBetween(1, 20))
                 ->setImage($faker->randomElement($speakerImages))
-                ;
-                array_push($speakerArray, $speaker);
-                $manager->persist($speaker);
+            ;
+            array_push($speakerArray, $speaker);
+            $manager->persist($speaker);
         }
 
         // Tableau de 20 événements
@@ -69,7 +84,7 @@ class AppFixtures extends Fixture
         ];
 
         // Boucle pour créer 20 événements
-        for ($i=0; $i < count($events); $i++) { 
+        for ($i = 0; $i < count($events); $i++) {
             $event = new Event();
             $event->setName($events[$i])
                 ->setTheme('Web development')
@@ -78,10 +93,32 @@ class AppFixtures extends Fixture
                 ->setAttendee($faker->numberBetween(10, 100))
                 ->setPrice($faker->numberBetween(0, 250))
                 ->addSpeaker($speakerArray[$i])
-                ;
+            ;
             $manager->persist($event);
         }
-        
+
+
+
+
+        // //  Création des utilisateurs
+        $userArray = [];
+        for ($i = 0; $i < 200; $i++) {
+            $userArray[] = $this->getReference('user_' . $i, User::class);
+        }
+
+        //  Création d'un loop qui permet de créer au moin 200 posts
+        for ($i = 0; $i < 200; $i++) {
+            $post = new Post();
+            $post->setTitle($faker->title());
+            $post->setContent($faker->paragraph());
+            $post->setImage(('https://picsum.photos/1280/720?random=' . $i));
+            $post->setIsPublished($faker->boolean());
+            $post->setUser($faker->randomElement($userArray));
+
+            $manager->persist($post);
+        }
+
+
         $manager->flush();
     }
 }
