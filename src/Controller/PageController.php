f86fa6d128088e3355837738fe8e7b27f90db607
LucasPort <lb.lucasbourdon@gmail.com>
2025-01-23 12:02:52 +0100

    Ajout de la pagination des posts dans le contrôleur de page

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,23 +3,31 @@
 namespace App\Controller;
 
 use App\Repository\PostRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PageController extends AbstractController
 {
-    #[Route('/posts', name: 'app_post', methods: ['GET'])] // Method get pour seulement récupérer les informations
-    public function posts(PostRepository $pr): Response
+    #[Route('/posts', name: 'app_posts', methods: ['GET'])] // Method get pour seulement récupérer les informations
+    public function posts(PostRepository $pr, PaginatorInterface $paginator, Request $request): Response
     {
+        $pagination = $paginator->paginate(
+            $pr->findAll(), /* query NOT result */
+            $request->query->getInt('page', 1), /* page number */
+            10 /* limit per page */
+        );
 
         $posts = $pr->findAll();
         return $this->render('page/posts.html.twig', [
-            'posts' => $posts,
+            'posts' => $pagination,
+            'pagination' => $pagination
         ]);
     }
 
-    #[Route('/posts/{id}', name: 'app_post_id', methods: ['GET'])] // Method get pour seulement récupérer les informations
+    #[Route('/post/{id}', name: 'app_post_id', methods: ['GET'])] // Method get pour seulement récupérer les informations
     public function postsId(PostRepository $pr, string $id): Response
     {
 
